e65658ac97f325c29c441d063eccf41cb10ae0b0
jacobg1 <jeg4t@virginia.edu>
2017-06-12 10:38:47 -0500

    more logic

--- wdi-fundamentals-memorygame/JS/main.js
+++ wdi-fundamentals-memorygame/JS/main.js
@@ -1,13 +1,21 @@
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
 
+var cardOne = cards[0];
 cardsInPlay.push(cardOne);
 cardsInPlay;
 console.log("User flipped queen");
 
-var cardTwo = [2];
+var cardTwo = cards[2];
 cardsInPlay.push(cardTwo);
 cardsInPlay;
 console.log("User flipped king");
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
