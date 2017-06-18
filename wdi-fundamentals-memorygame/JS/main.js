00237d18d09c666559e581f82a4ff597bf565724
jacobg1 <jeg4t@virginia.edu>
2017-06-18 17:12:45 -0500

    version 1.0

--- wdi-fundamentals-memorygame/JS/main.js
+++ wdi-fundamentals-memorygame/JS/main.js
@@ -32,8 +32,10 @@ var checkForMatch = function () {
 	}
 };
 
-var flipCard = function (cardId) {
+var flipCard = function () {
+	var cardId = this.getAttribute('data-id');
 	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
 	if (cardsInPlay.length === 2) checkForMatch();
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
@@ -41,8 +43,17 @@ var flipCard = function (cardId) {
 
 };
 
-flipCard(0);
-flipCard(2);
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+	document.getElementById('game-board').appendChild(cardElement);
+}
+};
+
+createBoard();
 
 
 	
